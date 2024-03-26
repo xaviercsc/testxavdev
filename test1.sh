9285aeb91def9aed31c8f3639ac455a586339d5d
xaviercsc <55680312+xaviercsc@users.noreply.github.com>
2024-03-27 00:25:22 +0530

    Create test1.sh

---
+++ test1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/your-username/your-repository.git
+cd your-repository
+
+# Create a new file
+touch new-file.txt
+
+# Echo the output of ls -ahl to the file
+ls -ahl > new-file.txt
+
+# Commit the changes
+git add .
+git commit -m "Adding new-file.txt and updating with ls -ahl output"
+
+# Push the changes back to the GitHub repository
+git push origin master
